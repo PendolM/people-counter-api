9247aef0c7968c1c706347d862d7f65b550518de
Marcin Pendolski <marcin.pendolski@gmail.com>
2024-01-28 10:15:13 +0100

    update file main.py

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+
 class HelloWorld2(Resource):
     def get(self):
         return {'hello': 'world2'}
+
 
 api.add_resource(HelloWorld, '/test')
 api.add_resource(HelloWorld2, '/test2')
