829d3febaee53fc2c92161dde04e6c8dfdc0dc71
Marcin Pendolski <marcin.pendolski@gmail.com>
2024-01-28 09:54:28 +0100

    add main file

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class HelloWorld2(Resource):
+    def get(self):
+        return {'hello': 'world2'}
+
+api.add_resource(HelloWorld, '/test')
+api.add_resource(HelloWorld2, '/test2')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
